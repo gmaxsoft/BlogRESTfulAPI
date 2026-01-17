2c9e78fb364a282a8dd9d42d7d41a9b8c22c814c
Grzegorz Miśków <133238725+gmaxsoft@users.noreply.github.com>
2026-01-17 18:53:44 +0100

    Fix e2e tests: use better-sqlite3 in-memory database for testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,18 +2,46 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './../src/app.controller';
+import { AppService } from './../src/app.service';
+import { UsersModule } from './../src/users/users.module';
+import { PostsModule } from './../src/posts/posts.module';
+import { CommentsModule } from './../src/comments/comments.module';
+import { AuthModule } from './../src/auth/auth.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+        TypeOrmModule.forRoot({
+          type: 'better-sqlite3',
+          database: ':memory:',
+          entities: [],
+          synchronize: true,
+          dropSchema: true,
+        }),
+        UsersModule,
+        PostsModule,
+        CommentsModule,
+        AuthModule,
+      ],
+      controllers: [AppController],
+      providers: [AppService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
